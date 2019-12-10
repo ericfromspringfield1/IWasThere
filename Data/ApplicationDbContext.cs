04b74376094901881119c02e47a9bccc69ecf3a6
ericfromspringfield1 <ericfromsrpingfield@gmail.com>
2019-12-10 13:14:40 -0600

    Seeding database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using IWasThere.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,5 +16,36 @@ namespace IWasThere.Data
 
         }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Game> Game { get; set; }
+        public DbSet<Team> Team { get; set; }
+        public DbSet<Location> Location { get; set; }
+        public DbSet<UserGame> UserGame { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+            ApplicationUser user = new ApplicationUser
+            {
+                FirstName = "Admina",
+                LastName = "Straytor",
+                UserName = "admin@admin.com",
+                NormalizedUserName = "ADMIN@ADMIN.COM",
+                Email = "admin@admin.com",
+                NormalizedEmail = "ADMIN@ADMIN.COM",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                SecurityStamp = "7f434309-a4d9-48e9-9ebb-8803db794577",
+                Id = "00000000-ffff-ffff-ffff-ffffffffffff"
+            };
+
+        var passwordHash = new PasswordHasher<ApplicationUser>();
+        user.PasswordHash = passwordHash.HashPassword(user, "Admin8*");
+            modelBuilder.Entity<ApplicationUser>().HasData(user);
+
+        }
+
     }
 }
--- Models/Game.cs
+++ Models/Game.cs
@@ -19,7 +19,6 @@ namespace IWasThere.Models
         public int HomeScore { get; set; }
         public int AwayTeamId { get; set; }
         public int AwayScore { get; set; }
-
-        //public virtual ICollection<UserGame> UserGames { get; set; }
+        public virtual ICollection<UserGame> UserGames { get; set; }
     }
 }
